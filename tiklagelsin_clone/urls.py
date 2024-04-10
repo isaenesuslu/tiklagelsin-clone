895ae75417560a491034598579499f8da3c3d7bd
İsa Enes Uslu <132603690+isaenesuslu@users.noreply.github.com>
2024-04-10 21:56:51 +0300

    Update

--- tiklagelsin_clone/urls.py
+++ tiklagelsin_clone/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Anasayfa, name="Anasayfa"),
+    path('ozel-menuler', ozelMenuler, name="ozelMenuler"),
     path('template', TemplatePage),
     path('burger', burgerPage),
     # BLOG
